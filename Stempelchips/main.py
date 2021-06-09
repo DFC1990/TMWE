80290c27e9675b070ae1a88c5a93449fbe141805
fernandez.daniel <daniel-fernandez@outlook.de>
2021-06-09 14:33:18 +0200

    test1

--- Stempelchips/main.py
+++ Stempelchips/main.py
@@ -1,9 +1,35 @@
 import pyodbc
+import sys
+import traceback
 
 
-conn = pyodbc.connect('Driver={SQL Server};'
-                                  'Server=192.168.12.1;'
-                                  'Database=ogkadenkasse_new;'
-                                  'UID=dfc;'
-                                  'PWD=daNiel,1611;'
-                                  'Trusted_Connection=no;')
+
+
+class Db:
+    DRIVER   =  r'DRIVER={SQL Server};'
+    SERVER   =  r'SERVER=172.25.2.40\SQLEXPRESS;'
+    DATABASE =  r'DATABASE=bcore;'
+    USERNAME =  r'UID=bcommerp;'
+    PASSWORD =  r'PWD=bcomm$01'
+
+    def __init__(self):
+        try:
+            self.cnxn = pyodbc.connect(self.DRIVER + self.SERVER + \
+                        self.DATABASE + self.USERNAME + self.PASSWORD)
+        except Exception:
+            self.__exit__(sys.exc_info())
+
+        self.cursor = self.cnxn.cursor()
+
+    def __enter__(self):
+        return self
+
+    #def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_msg):
+        print("__exit__")
+        print(exc_msg)
+        self.cursor.close()
+        self.cnxn.close()
+
+with Db() as d:
+    print(d)
